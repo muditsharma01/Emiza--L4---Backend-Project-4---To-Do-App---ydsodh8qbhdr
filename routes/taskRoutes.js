02f6a1f65854d9011e5b23fd7627830944bea130
Mudit Sharma <muditshar001@gmail.com>
2023-05-22 19:32:53 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
-
 const { createTask, getdetailTask, updateTask, deleteTask, getallTask } = require("../controllers/taskControllers");
-const { isowner } = require("../middleware/taskMiddleware.js");
+const { isowner } = require("../middleware/taskMiddleware");
 
 const router = express.Router();
 
 router.get("/", getallTask);
 router.post("/create", createTask);
-router.get("/detail", isowner , getdetailTask);
+router.get("/detail", isowner, getdetailTask);
 router.post("/delete", isowner, deleteTask);
 router.post("/update", isowner, updateTask);
 
